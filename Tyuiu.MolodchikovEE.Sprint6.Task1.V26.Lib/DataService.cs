2ea67b9e16d66fd101c9d9ac624b1b6786f22cfc
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-12 16:02:39 +0500

    Task1

--- Tyuiu.MolodchikovEE.Sprint6.Task1.V26.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task1.V26.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.MolodchikovEE.Sprint6.Task1.V26.Lib
             valueArray = new double[len];
             double y;
             int count = 0;
-            for (int i = startValue; i < stopValue; i++) 
+            for (int i = startValue; i <= stopValue; i++) 
             {
                 y = Math.Round((2 * Math.Cos(i) + 2)/(2 * i - 1) + Math.Cos(i) - 5 * i + 3,2);
                 valueArray[count] = y;
