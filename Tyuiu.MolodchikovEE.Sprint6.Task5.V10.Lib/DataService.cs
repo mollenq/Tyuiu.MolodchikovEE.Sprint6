bc6e258ba93b46ff2c511c5e63ff3fc1483e8ac4
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-13 17:39:19 +0500

    Task5

--- Tyuiu.MolodchikovEE.Sprint6.Task5.V10.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task5.V10.Lib/DataService.cs
@@ -4,25 +4,11 @@ namespace Tyuiu.MolodchikovEE.Sprint6.Task5.V10.Lib
 {
     public class DataService : ISprint6Task5V10
     {
-        public int len = 0;
         public double[] LoadFromDataFile(string path)
         {
-            using (StreamReader reader = new StreamReader(path)) 
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null) { len++; }
-            }
-
-            double [] numsArray = new double[len];
-
-            int index = 0;
-            using (StreamReader reader = new StreamReader(path)) 
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null) { numsArray[index] = Convert.ToDouble(line); len++; }
-            }
-
-            numsArray = numsArray.Where(val => val >0).ToArray();
+            string[] lines = File.ReadAllLines(path);
+            double[] numsArray = lines.Select(line => Convert.ToDouble(line)).ToArray();
+            numsArray = numsArray.Where(val => val != 0).ToArray();
             return numsArray;
         }
     }
