443b4cf6361df7815391a5ef7ee12eb258fbe398
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-13 20:42:25 +0500

    Task6

--- Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib/DataService.cs
@@ -1,26 +1,30 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib
 {
     public class DataService : ISprint6Task6V15
     {
         public string CollectTextFromFile(string str, string path)
-        {
+        {            
             string fullPath = Path.Combine(path, str);
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException($"Файл не найден: {fullPath}");
+            }
             string[] lines = File.ReadAllLines(fullPath);
-            string result = "";
-
+            StringBuilder result = new StringBuilder();
             foreach (string line in lines)
             {
-                string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
                 if (words.Length > 0)
                 {
-                    result += words[words.Length - 1] + " ";
+                    result.Append(words[^1] + " ");
                 }
             }
 
-            return result.Trim();
+            return result.ToString().Trim();
         }
     }
 }
