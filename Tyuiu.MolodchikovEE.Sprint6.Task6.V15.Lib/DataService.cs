b024c1e44796ceb17081e636c7b0158ea6cda304
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-13 20:24:20 +0500

    Task6

--- Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib/DataService.cs
@@ -6,8 +6,20 @@ namespace Tyuiu.MolodchikovEE.Sprint6.Task6.V15.Lib
     {
         public string CollectTextFromFile(string str, string path)
         {
-            string result = "Привет";
-            return result;
+            string[] lines = File.ReadAllLines(path);
+            string result = "";
+
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Length > 0)
+                {
+                    result += words[words.Length - 1] + " ";
+                }
+            }
+
+            return result.Trim();
         }
     }
 }
