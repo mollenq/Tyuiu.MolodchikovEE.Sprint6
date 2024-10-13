9de8e77fd93cc08f18e722efb54dbc62d86e7015
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-10-13 15:53:06 +0500

    Task4

--- Tyuiu.MolodchikovEE.Sprint6.Task4.V12/FormMain.Designer.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task4.V12/FormMain.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Tyuiu.MolodchikovEE.Sprint6.Task4.V12
+﻿using System.Windows.Forms;
+
+namespace Tyuiu.MolodchikovEE.Sprint6.Task4.V12
 {
     partial class FormMain
     {
@@ -28,12 +30,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBoxInfo = new GroupBox();
+            pictureBox = new PictureBox();
+            groupBoxPrint = new GroupBox();
+            label3 = new Label();
+            textBoxResult = new TextBox();
+            groupBoxInput = new GroupBox();
+            textBoxStop = new TextBox();
+            textBoxStart = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            buttonRun = new Button();
+            buttonHelp = new Button();
+            buttonSave = new Button();
+            groupBoxInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            groupBoxPrint.SuspendLayout();
+            groupBoxInput.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBoxInfo
+            // 
+            groupBoxInfo.Controls.Add(pictureBox);
+            groupBoxInfo.Font = new Font("Segoe UI", 12F);
+            groupBoxInfo.Location = new Point(12, 12);
+            groupBoxInfo.Name = "groupBoxInfo";
+            groupBoxInfo.Size = new Size(435, 282);
+            groupBoxInfo.TabIndex = 0;
+            groupBoxInfo.TabStop = false;
+            groupBoxInfo.Tag = "";
+            groupBoxInfo.Text = "Условие";
+            groupBoxInfo.UseCompatibleTextRendering = true;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Image = Properties.Resources.formula4;
+            pictureBox.Location = new Point(0, 31);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(435, 160);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.Click += pictureBox1_Click;
+            // 
+            // groupBoxPrint
+            // 
+            groupBoxPrint.Controls.Add(label3);
+            groupBoxPrint.Controls.Add(textBoxResult);
+            groupBoxPrint.Font = new Font("Segoe UI", 12F);
+            groupBoxPrint.Location = new Point(453, 12);
+            groupBoxPrint.Name = "groupBoxPrint";
+            groupBoxPrint.Size = new Size(299, 397);
+            groupBoxPrint.TabIndex = 1;
+            groupBoxPrint.TabStop = false;
+            groupBoxPrint.Text = "Вывод данных";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(6, 31);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Результат:";
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBoxResult.Location = new Point(6, 53);
+            textBoxResult.Multiline = true;
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.ReadOnly = true;
+            textBoxResult.ScrollBars = ScrollBars.Vertical;
+            textBoxResult.Size = new Size(287, 338);
+            textBoxResult.TabIndex = 0;
+            // 
+            // groupBoxInput
+            // 
+            groupBoxInput.Controls.Add(textBoxStop);
+            groupBoxInput.Controls.Add(textBoxStart);
+            groupBoxInput.Controls.Add(label2);
+            groupBoxInput.Controls.Add(label1);
+            groupBoxInput.Font = new Font("Segoe UI", 12F);
+            groupBoxInput.Location = new Point(12, 300);
+            groupBoxInput.Name = "groupBoxInput";
+            groupBoxInput.Size = new Size(237, 109);
+            groupBoxInput.TabIndex = 2;
+            groupBoxInput.TabStop = false;
+            groupBoxInput.Text = "Ввод данных";
+            groupBoxInput.Enter += groupBox3_Enter;
+            // 
+            // textBoxStop
+            // 
+            textBoxStop.Location = new Point(122, 53);
+            textBoxStop.Name = "textBoxStop";
+            textBoxStop.Size = new Size(85, 29);
+            textBoxStop.TabIndex = 3;
+            textBoxStop.Text = "5";
+            // 
+            // textBoxStart
+            // 
+            textBoxStart.Location = new Point(6, 53);
+            textBoxStart.Name = "textBoxStart";
+            textBoxStart.Size = new Size(82, 29);
+            textBoxStart.TabIndex = 2;
+            textBoxStart.Text = "-5";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(122, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Конец шага:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.Location = new Point(6, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Старт шага:";
+            // 
+            // buttonRun
+            // 
+            buttonRun.BackColor = Color.FromArgb(0, 192, 0);
+            buttonRun.Font = new Font("Segoe UI", 12F);
+            buttonRun.Location = new Point(342, 353);
+            buttonRun.Name = "buttonRun";
+            buttonRun.Size = new Size(105, 56);
+            buttonRun.TabIndex = 3;
+            buttonRun.Text = "Выполнить";
+            buttonRun.UseVisualStyleBackColor = false;
+            buttonRun.Click += buttonRunClick;
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.BackColor = Color.FromArgb(0, 192, 192);
+            buttonHelp.Font = new Font("Segoe UI", 12F);
+            buttonHelp.ForeColor = SystemColors.ControlText;
+            buttonHelp.Location = new Point(255, 353);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(81, 56);
+            buttonHelp.TabIndex = 4;
+            buttonHelp.Text = "Справка";
+            buttonHelp.UseVisualStyleBackColor = false;
+            buttonHelp.Click += buttonHelp_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.BackColor = Color.Yellow;
+            buttonSave.Font = new Font("Segoe UI", 12F);
+            buttonSave.Location = new Point(255, 300);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(192, 50);
+            buttonSave.TabIndex = 5;
+            buttonSave.Text = "Сохранить в файл";
+            buttonSave.UseVisualStyleBackColor = false;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(764, 421);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonHelp);
+            Controls.Add(buttonRun);
+            Controls.Add(groupBoxInput);
+            Controls.Add(groupBoxPrint);
+            Controls.Add(groupBoxInfo);
+            Font = new Font("Segoe UI", 9F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Спринт 6 | Таск 4 | Вариант 12 | Молодчиков Е.Е.";
+            groupBoxInfo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            groupBoxPrint.ResumeLayout(false);
+            groupBoxPrint.PerformLayout();
+            groupBoxInput.ResumeLayout(false);
+            groupBoxInput.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxInfo;
+        private GroupBox groupBoxPrint;
+        private GroupBox groupBoxInput;
+        private PictureBox pictureBox;
+        private TextBox textBoxResult;
+        private Button buttonRun;
+        private Button buttonHelp;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxStop;
+        private TextBox textBoxStart;
+        private Button buttonSave;
     }
 }
--- Tyuiu.MolodchikovEE.Sprint6.Task4.V12/FormMain.cs
+++ Tyuiu.MolodchikovEE.Sprint6.Task4.V12/FormMain.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms.VisualStyles;
 using Tyuiu.MolodchikovEE.Sprint6.Task4.V12.Lib;
 
 namespace Tyuiu.MolodchikovEE.Sprint6.Task4.V12
@@ -7,6 +8,92 @@ namespace Tyuiu.MolodchikovEE.Sprint6.Task4.V12
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+
+        private void buttonRunClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int startStep = Convert.ToInt32(textBoxStart.Text);
+                int stopStep = Convert.ToInt32(textBoxStop.Text);
+
+                string strLine;
+
+                int len = ds.GetMassFunction(startStep, stopStep).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+                valueArray = ds.GetMassFunction(startStep, stopStep);
+
+                textBoxResult.Text = "";
+                textBoxResult.AppendText("+----------+--------+" + Environment.NewLine);
+                textBoxResult.AppendText("|     X    |  f(X)  |" + Environment.NewLine);
+                textBoxResult.AppendText("+----------+--------+" + Environment.NewLine);
+
+                for (int i = 0; i <= len - 1; i++)
+                {
+                    strLine = String.Format("|{0,5:d}     |{1, 5:f2}   |", startStep, valueArray[i]);
+                    textBoxResult.AppendText(strLine + Environment.NewLine);
+                    startStep++;
+                }
+
+                textBoxResult.AppendText("+----------+--------+" + Environment.NewLine);
+
+            }
+            catch
+            {
+                MessageBox.Show("Введены невенрные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 4 выполнил студент группы ИИПб-24-1 Молодчиков Егор Евгеньевич", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void buttonHelp_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Таск 4 выполнил студент группы ИИПб-24-1 Молодчиков Егор Евгеньевич", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask4.txt";
+                File.WriteAllText(path, textBoxResult.Text);
+
+                DialogResult dialogResult = MessageBox.Show($"Файл {path} успешно сохранен! \n Открыть его в блокноте?", "Сообщение", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (dialogResult == DialogResult.Yes) 
+                {
+                    System.Diagnostics.Process txt = new System.Diagnostics.Process();
+                    txt.StartInfo.FileName = "notepad.exe";
+                    txt.StartInfo.Arguments = path;
+                    txt.Start();
+                }  
+            }
+            catch 
+            {
+                MessageBox.Show("Сбой при сохранение файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
